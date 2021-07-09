b8bf753f7051bd1c6679d764f6f82f4a66fa62b9
John Papandriopoulos <jpap@users.noreply.github.com>
2021-07-09 13:37:23 -0700

    Updated copyrights and moved package description to 00doc.go file.

--- grouped.go
+++ grouped.go
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.md file.
 
-// Package concurrent makes it easy to process a list of things
-// concurrently using a simple closure.
-package concurrent // import "go.jpap.org/concurrent"
+package concurrent
 
 import (
 	"runtime"
--- runner.go
+++ runner.go
@@ -1,3 +1,7 @@
+// Copyright 2021 John Papandriopoulos.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.md file.
+
 package concurrent
 
 import (
